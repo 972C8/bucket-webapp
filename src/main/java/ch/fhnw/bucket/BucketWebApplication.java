c900ba99f3720fd022fee285f60fcb4e890048c3
972C8 <me@nineseven.one>
2022-01-07 11:26:33 +0100

    Refactor ACRM to Bucket

--- src/main/java/ch/fhnw/bucket/BucketWebApplication.java
+++ src/main/java/ch/fhnw/bucket/BucketWebApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ACRMWebApplication {
+public class BucketWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ACRMWebApplication.class, args);
+        SpringApplication.run(BucketWebApplication.class, args);
     }
 }
