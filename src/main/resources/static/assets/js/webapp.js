ec44d0f251d337c79e2d9d532b219e800bd28cce
Marco Kaufmann <marcokaufmann@malako.xyz>
2022-01-10 10:54:43 +0100

    Update register & login

--- src/main/resources/static/assets/js/webapp.js
+++ src/main/resources/static/assets/js/webapp.js
@@ -1,34 +1,11 @@
 serviceEndpointURL = window.location.protocol + '//' + window.location.host;
 
-async function login(email, password, remember, callback) {
-  const result = await Request.POST('/login', {
-    email: email,
-    password: password,
-    remember: remember,
-  });
-  callback(result);
-}
-
 async function validateLogin(callback) {
   try {
     await Request.HEAD('/validate');
     callback(true);
   } catch (e) {
     callback(false);
-  }
-}
-
-async function register(name, email, password, callbackSuccess, callbackError) {
-  try {
-    await Request.POST('/user/register', {
-      name: name,
-      email: email,
-      password: password,
-    });
-
-    callbackSuccess(true);
-  } catch (e) {
-    callbackError(e);
   }
 }
 
